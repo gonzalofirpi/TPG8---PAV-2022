a403f4b4f1dd32ec0c4f2abd727677d28b52a99a
gonzalofirpi <107421729+gonzalofirpi@users.noreply.github.com>
2022-10-01 20:53:55 -0300

    menu

--- TrabajoIntegradorG8/Principal.Designer.cs
+++ TrabajoIntegradorG8/Principal.Designer.cs
@@ -28,20 +28,304 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.clubesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aBMClubesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sociosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aBMSociosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.canchasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aBMCanchasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jugadoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.arbitrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.categoriasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.comisionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.arbitrosToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.barriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.localidadesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.arbitrosXPartidoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.observacionesXPartidoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.observacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem7 = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem8 = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem9 = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultasToolStripMenuItem10 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clubesToolStripMenuItem,
+            this.sociosToolStripMenuItem,
+            this.canchasToolStripMenuItem,
+            this.jugadoresToolStripMenuItem,
+            this.arbitrosToolStripMenuItem,
+            this.categoriasToolStripMenuItem,
+            this.comisionesToolStripMenuItem,
+            this.arbitrosToolStripMenuItem1,
+            this.cargosToolStripMenuItem,
+            this.barriosToolStripMenuItem,
+            this.localidadesToolStripMenuItem,
+            this.arbitrosXPartidoToolStripMenuItem,
+            this.observacionesXPartidoToolStripMenuItem,
+            this.observacionesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(972, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // clubesToolStripMenuItem
+            // 
+            this.clubesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aBMClubesToolStripMenuItem});
+            this.clubesToolStripMenuItem.Name = "clubesToolStripMenuItem";
+            this.clubesToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.clubesToolStripMenuItem.Text = "Clubes";
+            // 
+            // aBMClubesToolStripMenuItem
+            // 
+            this.aBMClubesToolStripMenuItem.Name = "aBMClubesToolStripMenuItem";
+            this.aBMClubesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aBMClubesToolStripMenuItem.Text = "Consultas";
+            // 
+            // sociosToolStripMenuItem
+            // 
+            this.sociosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aBMSociosToolStripMenuItem});
+            this.sociosToolStripMenuItem.Name = "sociosToolStripMenuItem";
+            this.sociosToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.sociosToolStripMenuItem.Text = "Comisiones";
+            // 
+            // aBMSociosToolStripMenuItem
+            // 
+            this.aBMSociosToolStripMenuItem.Name = "aBMSociosToolStripMenuItem";
+            this.aBMSociosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aBMSociosToolStripMenuItem.Text = "Consultas";
+            // 
+            // canchasToolStripMenuItem
+            // 
+            this.canchasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aBMCanchasToolStripMenuItem});
+            this.canchasToolStripMenuItem.Name = "canchasToolStripMenuItem";
+            this.canchasToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.canchasToolStripMenuItem.Text = "Canchas";
+            // 
+            // aBMCanchasToolStripMenuItem
+            // 
+            this.aBMCanchasToolStripMenuItem.Name = "aBMCanchasToolStripMenuItem";
+            this.aBMCanchasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aBMCanchasToolStripMenuItem.Text = "Consultas";
+            // 
+            // jugadoresToolStripMenuItem
+            // 
+            this.jugadoresToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultasToolStripMenuItem});
+            this.jugadoresToolStripMenuItem.Name = "jugadoresToolStripMenuItem";
+            this.jugadoresToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.jugadoresToolStripMenuItem.Text = "Socios";
+            // 
+            // consultasToolStripMenuItem
+            // 
+            this.consultasToolStripMenuItem.Name = "consultasToolStripMenuItem";
+            this.consultasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.consultasToolStripMenuItem.Text = "Consultas";
+            // 
+            // arbitrosToolStripMenuItem
+            // 
+            this.arbitrosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultasToolStripMenuItem1});
+            this.arbitrosToolStripMenuItem.Name = "arbitrosToolStripMenuItem";
+            this.arbitrosToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.arbitrosToolStripMenuItem.Text = "Jugadores";
+            // 
+            // consultasToolStripMenuItem1
+            // 
+            this.consultasToolStripMenuItem1.Name = "consultasToolStripMenuItem1";
+            this.consultasToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.consultasToolStripMenuItem1.Text = "Consultas";
+            // 
+            // categoriasToolStripMenuItem
+            // 
+            this.categoriasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultasToolStripMenuItem2});
+            this.categoriasToolStripMenuItem.Name = "categoriasToolStripMenuItem";
+            this.categoriasToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.categoriasToolStripMenuItem.Text = "Categorias";
+            // 
+            // consultasToolStripMenuItem2
+            // 
+            this.consultasToolStripMenuItem2.Name = "consultasToolStripMenuItem2";
+            this.consultasToolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.consultasToolStripMenuItem2.Text = "Consultas";
+            // 
+            // comisionesToolStripMenuItem
+            // 
+            this.comisionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultasToolStripMenuItem3});
+            this.comisionesToolStripMenuItem.Name = "comisionesToolStripMenuItem";
+            this.comisionesToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.comisionesToolStripMenuItem.Text = "Puestos";
+            // 
+            // consultasToolStripMenuItem3
+            // 
+            this.consultasToolStripMenuItem3.Name = "consultasToolStripMenuItem3";
+            this.consultasToolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.consultasToolStripMenuItem3.Text = "Consultas";
+            // 
+            // arbitrosToolStripMenuItem1
+            // 
+            this.arbitrosToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultasToolStripMenuItem10});
+            this.arbitrosToolStripMenuItem1.Name = "arbitrosToolStripMenuItem1";
+            this.arbitrosToolStripMenuItem1.Size = new System.Drawing.Size(61, 20);
+            this.arbitrosToolStripMenuItem1.Text = "Arbitros";
+            // 
+            // cargosToolStripMenuItem
+            // 
+            this.cargosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultasToolStripMenuItem9});
+            this.cargosToolStripMenuItem.Name = "cargosToolStripMenuItem";
+            this.cargosToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.cargosToolStripMenuItem.Text = "Cargos";
+            // 
+            // barriosToolStripMenuItem
+            // 
+            this.barriosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultasToolStripMenuItem8});
+            this.barriosToolStripMenuItem.Name = "barriosToolStripMenuItem";
+            this.barriosToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.barriosToolStripMenuItem.Text = "Barrios";
+            // 
+            // localidadesToolStripMenuItem
+            // 
+            this.localidadesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultasToolStripMenuItem7});
+            this.localidadesToolStripMenuItem.Name = "localidadesToolStripMenuItem";
+            this.localidadesToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.localidadesToolStripMenuItem.Text = "Localidades";
+            // 
+            // arbitrosXPartidoToolStripMenuItem
+            // 
+            this.arbitrosXPartidoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultasToolStripMenuItem6});
+            this.arbitrosXPartidoToolStripMenuItem.Name = "arbitrosXPartidoToolStripMenuItem";
+            this.arbitrosXPartidoToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.arbitrosXPartidoToolStripMenuItem.Text = "Usuarios";
+            // 
+            // observacionesXPartidoToolStripMenuItem
+            // 
+            this.observacionesXPartidoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultasToolStripMenuItem5});
+            this.observacionesXPartidoToolStripMenuItem.Name = "observacionesXPartidoToolStripMenuItem";
+            this.observacionesXPartidoToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.observacionesXPartidoToolStripMenuItem.Text = "Tipos Piso";
+            // 
+            // observacionesToolStripMenuItem
+            // 
+            this.observacionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consultasToolStripMenuItem4});
+            this.observacionesToolStripMenuItem.Name = "observacionesToolStripMenuItem";
+            this.observacionesToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.observacionesToolStripMenuItem.Text = "Observaciones";
+            // 
+            // consultasToolStripMenuItem4
+            // 
+            this.consultasToolStripMenuItem4.Name = "consultasToolStripMenuItem4";
+            this.consultasToolStripMenuItem4.Size = new System.Drawing.Size(180, 22);
+            this.consultasToolStripMenuItem4.Text = "Consultas";
+            // 
+            // consultasToolStripMenuItem5
+            // 
+            this.consultasToolStripMenuItem5.Name = "consultasToolStripMenuItem5";
+            this.consultasToolStripMenuItem5.Size = new System.Drawing.Size(180, 22);
+            this.consultasToolStripMenuItem5.Text = "Consultas";
+            // 
+            // consultasToolStripMenuItem6
+            // 
+            this.consultasToolStripMenuItem6.Name = "consultasToolStripMenuItem6";
+            this.consultasToolStripMenuItem6.Size = new System.Drawing.Size(180, 22);
+            this.consultasToolStripMenuItem6.Text = "Consultas";
+            // 
+            // consultasToolStripMenuItem7
+            // 
+            this.consultasToolStripMenuItem7.Name = "consultasToolStripMenuItem7";
+            this.consultasToolStripMenuItem7.Size = new System.Drawing.Size(180, 22);
+            this.consultasToolStripMenuItem7.Text = "Consultas";
+            // 
+            // consultasToolStripMenuItem8
+            // 
+            this.consultasToolStripMenuItem8.Name = "consultasToolStripMenuItem8";
+            this.consultasToolStripMenuItem8.Size = new System.Drawing.Size(180, 22);
+            this.consultasToolStripMenuItem8.Text = "Consultas";
+            // 
+            // consultasToolStripMenuItem9
+            // 
+            this.consultasToolStripMenuItem9.Name = "consultasToolStripMenuItem9";
+            this.consultasToolStripMenuItem9.Size = new System.Drawing.Size(180, 22);
+            this.consultasToolStripMenuItem9.Text = "Consultas";
+            // 
+            // consultasToolStripMenuItem10
+            // 
+            this.consultasToolStripMenuItem10.Name = "consultasToolStripMenuItem10";
+            this.consultasToolStripMenuItem10.Size = new System.Drawing.Size(180, 22);
+            this.consultasToolStripMenuItem10.Text = "Consultas";
             // 
             // frmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(972, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Principal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clubesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aBMClubesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sociosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aBMSociosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem canchasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aBMCanchasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jugadoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem arbitrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem categoriasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem comisionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem arbitrosToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem10;
+        private System.Windows.Forms.ToolStripMenuItem cargosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem9;
+        private System.Windows.Forms.ToolStripMenuItem barriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem8;
+        private System.Windows.Forms.ToolStripMenuItem localidadesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem7;
+        private System.Windows.Forms.ToolStripMenuItem arbitrosXPartidoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem6;
+        private System.Windows.Forms.ToolStripMenuItem observacionesXPartidoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem5;
+        private System.Windows.Forms.ToolStripMenuItem observacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultasToolStripMenuItem4;
     }
 }
